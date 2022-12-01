cec37f186e9ae4d72929e8e6515a960146383c04
schrutek <martin@schrutek>
2022-12-01 11:06:10 +0100

    Post Request & js-Directory added

--- layout - Kopie.php
+++ layout - Kopie.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <head>
 <body>
+<<<<<<< HEAD
     <div class="container">
         <h1>Welcome to WMC!</h1>
         <p>The great PHP & JS Sample...</p>
@@ -18,5 +19,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
     <script src="js/product.js"></script>
+=======
+    <h1>Welcome to WMC! (Git Tutorial)</h1>
+    <p>The great PHP & JS Sample...</p>
+    <?php
+        renderBody();
+    ?>
+>>>>>>> 9546af7b70234e3fd290fe842e319e64babd7327
 </body>
 </html>
